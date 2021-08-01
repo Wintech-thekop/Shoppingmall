b79225e25bda0a89d06bad86048fac39e395dab2
wintech <wintech_thekop@hotmail.com>
2021-08-01 22:08:17 +0700

    route

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  // General
+  static String appName = 'Shopping Mall';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeRiderService = '/riderService';
+  static String routeSalerService = '/salerService';
+}
