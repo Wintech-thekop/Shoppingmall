23228e15f0cfadb0113e3ca0aed5ea766ed7c25d
wintech <wintech_thekop@hotmail.com>
2021-08-02 06:29:51 +0700

    TextWidget

--- lib/state/authen.dart
+++ lib/state/authen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
 import 'package:shoppingmall/widgets/show_image.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
@@ -15,11 +16,37 @@ class _AuthenState extends State<Authen> {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: size*0.6,
-          child: ShowImage(path: MyConstant.image2),
+        child: ListView(
+          children: [
+            buildImage(size),
+            buildAppName(),
+          ],
         ),
       ),
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ShowTitle(
+                title: MyConstant.appName,
+                textStyle: MyConstant().h1Style(),
+              ),
+            ],
+          );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(path: MyConstant.image2),
+        ),
+      ],
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -16,4 +16,28 @@ class MyConstant {
   static String image2 = 'images/image2.png';
   static String image3 = 'images/image3.png';
   static String image4 = 'images/image4.png';
+
+  // Color
+  static Color primary = Color(0xffec407a);
+  static Color dark = Color(0xffb4004e);
+  static Color light = Color(0xffff77a9);
+
+  // Style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+
+      TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+
+      TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
 }
