1554df66676cbaa36261caec628086514258115a
wintech <wintech_thekop@hotmail.com>
2021-08-02 22:17:38 +0700

    FocusScope

--- lib/state/authen.dart
+++ lib/state/authen.dart
@@ -26,9 +26,7 @@ class _AuthenState extends State<Authen> {
               buildImage(size),
               buildAppName(),
               buildUser(size),
-              buildPassword(
-                size,
-              )
+              buildPassword(size),
             ],
           ),
         ),
