1281d71a1f3f4ee3a1efdb4df41575e1f8380c34
wintech <wintech_thekop@hotmail.com>
2021-08-21 16:39:40 +0700

    การใช้งาน TextEditControler ด้วย Model

--- lib/state/seller/edit_product.dart
+++ lib/state/seller/edit_product.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shoppingmall/models/product_model.dart';
+import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class EditProduct extends StatefulWidget {
   final ProductModel productModel;
@@ -11,6 +13,9 @@ class EditProduct extends StatefulWidget {
 
 class _EditProductState extends State<EditProduct> {
   ProductModel? productModel;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController detailController = TextEditingController();
 
   @override
   void initState() {
@@ -18,6 +23,9 @@ class _EditProductState extends State<EditProduct> {
 
     productModel = widget.productModel;
     print('## name Edit ==> ${productModel!.name}');
+    nameController.text = productModel!.name;
+    priceController.text = productModel!.price;
+    detailController.text = productModel!.detail;
   }
 
   @override
@@ -26,6 +34,88 @@ class _EditProductState extends State<EditProduct> {
       appBar: AppBar(
         title: Text('This is Edit Product'),
       ),
+      body: LayoutBuilder(
+        builder: (context, constraints) => Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildTitle('General :'),
+              buildName(constraints),
+              buildPrice(constraints),
+              buildDetail(constraints),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildName(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: constraints.maxWidth * 0.75,
+          child: TextFormField(
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Name :',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPrice(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          width: constraints.maxWidth * 0.75,
+          child: TextFormField(
+            controller: priceController,
+            decoration: InputDecoration(
+              labelText: 'Price :',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildDetail(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: constraints.maxWidth * 0.75,
+          child: TextFormField(
+            controller: detailController,
+            decoration: InputDecoration(
+              labelText: 'Detail :',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildTitle(String title) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ShowTitle(
+            title: title,
+            textStyle: MyConstant().h2Style(),
+          ),
+        ),
+      ],
     );
   }
 }
