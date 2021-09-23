dfd79704b22c4e70c5025173153ab1d318b4180e
wintech <wintech_thekop@hotmail.com>
2021-09-24 06:11:10 +0700

    การออกแบบ UI สำหรับแสดง List Product

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyConstant {
   // General
   static String appName = 'Shopping Mall';
-  static String domain = 'https://2968-2001-fb1-157-b655-848d-3e29-8f8b-454.ngrok.io';
+  static String domain = 'https://ec29-2001-fb1-157-b655-fc46-63c0-e0d2-24a4.ngrok.io';
 
   // Route
   static String routeAuthen = '/authen';
